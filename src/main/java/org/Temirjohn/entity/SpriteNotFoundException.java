cd3ca1f0ac5f49edc642b02ee5048cee16202ce9
TemirJohn <timur.dzhuldasov@outlook.com>
2025-05-06 20:13:20 +0500

    Implement SpriteNotFoundException.java

---
+++ src/main/java/org/Temirjohn/entity/SpriteNotFoundException.java
@@ -0,0 +1,9 @@
+package org.Temirjohn.entity;
+
+import java.io.IOException;
+
+public class SpriteNotFoundException extends IOException {
+    public SpriteNotFoundException(String path) {
+        super("Sprite could not be found at " + path + ".");
+    }
+}
