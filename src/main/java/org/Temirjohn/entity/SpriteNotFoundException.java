100f86740a2742c7dcbb5754d30290bc898456cb
TemirJohn <timur.dzhuldasov@outlook.com>
2025-05-07 18:11:55 +0500

    add commit

--- src/main/java/org/Temirjohn/entity/SpriteNotFoundException.java
+++ src/main/java/org/Temirjohn/entity/SpriteNotFoundException.java
@@ -2,7 +2,15 @@ package org.Temirjohn.entity;
 
 import java.io.IOException;
 
+/**
+ * {@summary Custom exception for when an entity's sprite cannot be found.}
+ */
+
 public class SpriteNotFoundException extends IOException {
+
+    /**
+     * @param path File path of sprite that could not be found.
+     */
     public SpriteNotFoundException(String path) {
         super("Sprite could not be found at " + path + ".");
     }
