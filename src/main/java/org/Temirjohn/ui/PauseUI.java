701b316bc2aac75cc3db6710f63d1e13b1bad369
TemirJohn <timur.dzhuldasov@outlook.com>
2025-05-08 00:22:36 +0500

    Add new class in ui directory

---
+++ src/main/java/org/Temirjohn/ui/PauseUI.java
@@ -0,0 +1,17 @@
+package org.Temirjohn.ui;
+
+import org.Temirjohn.main.GamePanel;
+import org.Temirjohn.main.Main;
+import org.Temirjohn.main.MainMenuState;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * UI class used to create the pause menu
+ */
+public class PauseUI {
+
+}
