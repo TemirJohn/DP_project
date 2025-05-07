ee405bce25910de52f43e4aa5de99478661dc8cd
d1rannn <denchik.stars06@gmail.com>
2025-05-07 23:21:08 +0500

    implement LevelFactory.java

--- src/main/java/org/Temirjohn/levels/LevelFactory.java
+++ src/main/java/org/Temirjohn/levels/LevelFactory.java
@@ -1,5 +1,31 @@
 package org.Temirjohn.levels;
 
-public class LevelFactory {
-    
+/**
+ * Creates levels
+ */
+public class LevelFactory implements iLevelFactory{
+    /**
+     * Constructor that takes a level number
+     * @param levelNum Integer representing a level number
+     */
+    public LevelFactory(int levelNum) { createLevel(levelNum); }
+
+    /**
+     * Creates a new level
+     * @param levelNum The level number
+     * @return A new level
+     */
+    @Override
+    public iLevel createLevel(int levelNum) {
+        switch (levelNum) {
+            case '1':
+                return new Level1();
+            case '2':
+                return new Level2();
+            case '3':
+                return new Level3();
+            default:
+                return null;
+        }
+    }
 }
