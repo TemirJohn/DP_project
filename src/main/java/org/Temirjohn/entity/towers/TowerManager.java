5c3dd8d3866b588a4f8cd08e3cb4bf898bff0014
TemirJohn <timur.dzhuldasov@outlook.com>
2025-05-07 17:58:34 +0500

    Implement TowerManager.java.

---
+++ src/main/java/org/Temirjohn/entity/towers/TowerManager.java
@@ -0,0 +1,14 @@
+package org.Temirjohn.entity.towers;
+
+
+
+/**
+ * This class acts as a composite, allowing GamePanel to update and draw all the player's towers at once rather
+ * than having to loop through each one.
+ *
+ *
+ */
+public class TowerManager implements ITower {
+
+
+}
