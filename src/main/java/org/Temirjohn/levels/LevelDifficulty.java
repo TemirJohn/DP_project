f6ee34f6a143c68286e8f9d72ceb7c1b947793ac
TemirJohn <timur.dzhuldasov@outlook.com>
2025-05-07 18:06:39 +0500

    Implement LevelDifficulty.java

---
+++ src/main/java/org/Temirjohn/levels/LevelDifficulty.java
@@ -0,0 +1,8 @@
+package org.Temirjohn.levels;
+
+/**
+ * Enum to hold difficulty-based stats. Should have been combined with Difficulty.java but we ran out of time
+ */
+public enum LevelDifficulty {
+
+}
